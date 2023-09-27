741ff15d7a3dd35bf9801e4d85338adc16cd252d
Pedro Figueiredo <pedro.figueiredo35@gmail.com>
2023-09-27 09:56:16 +0100

    update time model

--- models/Tournament.model.js
+++ models/Tournament.model.js
@@ -1,29 +1,35 @@
 const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 
-const tournamentSchema = new Schema({
-  title: {
-    type: String,
-    required: [true, "Title required"],
-  },
-  description: String,
-  prize: String,
-  category: {
-    type: String,
-    enum: ["single elimination", "double elimination", "triple elimination"],
-  },
-  participants: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  game: {
-    type: Schema.Types.ObjectId,
-    ref: "Game",
-  },
+const tournamentSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title required"],
+    },
+    description: String,
+    prize: String,
+    category: {
+      type: String,
+      enum: ["Single elimination", "Double elimination", "Triple elimination"],
+    },
+    participants: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    game: {
+      type: Schema.Types.ObjectId,
+      ref: "Game",
+    },
 
-  dateTime: {
-    $gte: ISODate("2023-09-27T00:00:00Z"),
+    dateTime: {
+      type: Date,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = model("Tournament", tournamentSchema);
